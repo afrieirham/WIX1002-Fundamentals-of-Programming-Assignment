9159633c8dce8ac4806311913fd5281b0f22f1b1
Afrie Irham <32460534+afrieirhamakram@users.noreply.github.com>
2017-12-13 14:43:54 +0800

    Blackjack 7.1

--- src/blackjack/pkg7/pkg0/Blackjack70.java
+++ src/blackjack/pkg7/pkg0/Blackjack70.java
@@ -9,13 +9,24 @@ public class Blackjack70 {
 
     public static void main(String[] args) {
 
+        System.out.println("Welcome to Blackjack!");
+        System.out.print("Please enter your name: ");
+        Scanner userName = new Scanner(System.in);
+        String name = userName.next();
+
         int money = 1000;
         boolean gameEnd = false;
         int bet = 0;
 
+        Deck playingCard = new Deck();
+        playingCard.createFullDeck();
+        playingCard.shuffleCard();
+
         while(true){
             do{
-                System.out.print("Bet: ");
+                System.out.print(name);
+                System.out.println(", you have RM" + money + ".");
+                System.out.print("Bet: RM");
                 Scanner input = new Scanner(System.in);
                 bet = input.nextInt();
 
@@ -25,7 +36,7 @@ public class Blackjack70 {
             }while(bet>money);
             if(bet == 0){break;}
 
-            boolean game = playBlackjack();
+            boolean game = playBlackjack(name);
 
             if(game){
                 //Player Wins
@@ -43,14 +54,9 @@ public class Blackjack70 {
         }
     }
 
-    public static boolean playBlackjack(){
-        Deck playingCard = new Deck();
+    public static boolean playBlackjack(String name){
 
-        playingCard.createFullDeck();
-        playingCard.shuffleCard();
-
-
-        Player P1 = new Player();
+        Player P1 = new Player(name);
         Player P2 = new Player();
 
         P1.drawCard();
@@ -59,17 +65,73 @@ public class Blackjack70 {
         P2.drawCard();
         P2.drawCard();
 
-        System.out.println("P1:");
-        P1.getCard();
-        System.out.println("P2:");
-        P2.getCard();
-        
-        P1.totalValue();
-        P2.totalValue();
-        
-        System.out.println("Total P1 = " + P1.totalValue());
-        System.out.println("Total P2 = " + P2.totalValue());
+        int playerValue = P1.totalValue();
+        int dealerValue = P2.totalValue();
 
+        if(playerValue == 21){
+            System.out.println("You Win");
+            System.out.println("You got a Blackjack!");
+            System.out.println("Your card:");
+            P1.getAllCard();
+            return true;
+
+        }
+        if(dealerValue == 21){
+            System.out.println("You Lose");
+            System.out.println("Dealer got a Blackjack!");
+            System.out.println("Dealer's card:");
+            P2.getAllCard();
+            return false;
+        }
+
+        while(true){
+            System.out.println("");
+            System.out.println("");
+            System.out.println(P1.name + ": Your Hand ");
+            System.out.println("--------------");
+            P1.getAllCard();
+            P1.totalValue();
+            System.out.println("Total Hand = " + P1.totalValue());
+            System.out.println("--------------");
+
+            System.out.println("");
+            System.out.println("");
+            System.out.println("Dealer:");
+            System.out.println("--------------");
+            P2.getCard(0);
+            P2.totalValue();
+            System.out.println("[HIDDEN]");
+            System.out.println("--------------");
+            System.out.println("");
+            System.out.println("");
+            Scanner input = new Scanner(System.in);
+            int userMove;
+            do{
+                System.out.println("1 - Hit");
+                System.out.println("2 - Stand");
+                userMove = input.nextInt();
+                if(userMove != 1 && userMove != 2){
+                    System.out.println("Please make a valid move.");
+                }
+            }while(userMove != 1 && userMove != 2);
+
+            if(userMove == 2){
+                break;
+            }
+            if(userMove == 1){
+                P1.drawCard();
+                P1.totalValue();
+                if(P1.totalValue() > 21){
+                    System.out.println(name + ", you bust!");
+                    System.out.println("--------------");
+                    P1.getAllCard();
+                    System.out.println("Total Hand = " + P1.totalValue());
+                    System.out.println("--------------");
+                    return false;
+                }
+            }
+
+        }
 
 
         return false;
--- src/blackjack/pkg7/pkg0/Player.java
+++ src/blackjack/pkg7/pkg0/Player.java
@@ -18,9 +18,14 @@ public class Player {
 
     private int i = 0;
     private int cardUsed = 0;
+    public String name;
 
+    public Player(String name){
+        this.name = name;
+    }
+    
     public Player(){
-
+        
     }
 
     public void drawCard(){
@@ -30,57 +35,43 @@ public class Player {
         cardUsed++;
     }
 
-    public void getCard(){
+    public void getAllCard(){
         for(int i=0 ; i<cardUsed ; i++){
             System.out.println(getCardSymbol(hands[i]));
         }
+    }
+    
+    public void getCard(int i){
+        System.out.println(getCardSymbol(hands[i]));
     }
 
     private String getCardSymbol(int index){
         String message = null;
         if(index>=40){
             index -= 39;
-            switch(index){
-                case 1:   message = "Ace"; break;
-                case 2:   message = "2"; break;
-                case 3:   message = "3"; break;
-                case 4:   message = "4"; break;
-                case 5:   message = "5"; break;
-                case 6:   message = "6"; break;
-                case 7:   message = "7"; break;
-                case 8:   message = "8"; break;
-                case 9:   message = "9"; break;
-                case 10:  message = "10"; break;
-                case 11:  message = "Jack"; break;
-                case 12:  message = "Queen"; break;
-                case 13:  message = "King"; break;
-            }
-            
+            message = getCardNumber(index);
             message += " of Diamond";
         }
         else if(index>=27){
             index -= 26;
-            switch(index){
-                case 1:   message = "Ace"; break;
-                case 2:   message = "2"; break;
-                case 3:   message = "3"; break;
-                case 4:   message = "4"; break;
-                case 5:   message = "5"; break;
-                case 6:   message = "6"; break;
-                case 7:   message = "7"; break;
-                case 8:   message = "8"; break;
-                case 9:   message = "9"; break;
-                case 10:  message = "10"; break;
-                case 11:  message = "Jack"; break;
-                case 12:  message = "Queen"; break;
-                case 13:  message = "King"; break;
-            }
-            
+            message = getCardNumber(index);
             message += " of Clubs";
         }
         else if(index>=14){
             index -= 13;
-            switch(index){
+            message = getCardNumber(index);
+            message += " of Hearts";
+        }
+        else{   
+        message = getCardNumber(index);
+        message += " of Spades";
+        }
+    return message;
+    }
+    
+    private String getCardNumber(int index){
+        String message = null;
+        switch(index){
                 case 1:   message = "Ace"; break;
                 case 2:   message = "2"; break;
                 case 3:   message = "3"; break;
@@ -94,32 +85,7 @@ public class Player {
                 case 11:  message = "Jack"; break;
                 case 12:  message = "Queen"; break;
                 case 13:  message = "King"; break;
-            }
-            
-            message += " of Hearts";
         }
-        else{
-           
-        switch(index){
-            case 1:   message = "Ace"; break;
-            case 2:   message = "2"; break;
-            case 3:   message = "3"; break;
-            case 4:   message = "4"; break;
-            case 5:   message = "5"; break;
-            case 6:   message = "6"; break;
-            case 7:   message = "7"; break;
-            case 8:   message = "8"; break;
-            case 9:   message = "9"; break;
-            case 10:  message = "10"; break;
-            case 11:  message = "Jack"; break;
-            case 12:  message = "Queen"; break;
-            case 13:  message = "King"; break;
-        }
-
-        message += " of Spades";
-        }
-
-
         return message;
     }
 
@@ -127,11 +93,12 @@ public class Player {
         int total = 0;
         int aceCounter = 0;
         for(int i=0; i<cardUsed ; i++){
-            if(cardValue(hands[i]) == 1 || cardValue(hands[i]) == 14 || cardValue(hands[i]) == 27 || cardValue(hands[i]) == 40 ){
+            int cardValue = cardValue(hands[i]);
+            if(cardValue == 1 || cardValue == 14 || cardValue == 27 || cardValue == 40 ){
                 aceCounter++;
             }
             else{
-                total += cardValue(hands[i]);
+                total += cardValue;
             }
 
         }
@@ -180,9 +147,14 @@ public class Player {
             case 40: value = 1;
             break;
 
-            default: break;
+            default: value = cardNumberValue(index);
+            break;
         }
-
+        return value;
+    }
+    
+    private int cardNumberValue(int index){
+        int value = 0;
         if(index>40){
             index -= 39;
             value = index;
@@ -195,61 +167,12 @@ public class Player {
             index -= 13;
             value = index;
         }
-
+        else{
+            value = index;
+        }
+        
         return value;
     }
-
-
-
-
-//    public int getCardValue(){
-//        int aceCount = 0;
-//        int value = 0;
-//        for(int i=0 ; i<10 ; i++){
-//            int test = hands[i];
-//
-//            switch(test){
-//                case 1:
-//                case 14:
-//                case 27:
-//                case 40: aceCount++;
-//                break;
-//
-//                case 10:
-//                case 11:
-//                case 12:
-//                case 13:
-//
-//                case 23:
-//                case 24:
-//                case 25:
-//                case 26:
-//
-//                case 36:
-//                case 37:
-//                case 38:
-//                case 39:
-//
-//                case 49:
-//                case 50:
-//                case 51:
-//                case 52:  value = 10;
-//
-//                default: break;
-//            }
-//
-//            if(test>40){
-//                test -= 39;
-//            }
-//            else if(test>27){
-//                test -= 26;
-//            }
-//            else if(test>14){
-//                test -= 13;
-//            }
-//
-//        }
-//    }
 
 
 }
